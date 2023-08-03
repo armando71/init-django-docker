2eed0a2a41d5dbd0676dba47794086cbe11324f2
armando71 <harmando71@gmail.com>
2023-08-03 11:19:01 -0600

    Modelo actualizado

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Task
+from api.models import Task, Comment
 
 # Register your models here.
 @admin.register(Task)
-
 class TaskAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'priority', 'crated_at']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task', 'comment', 'crated_at']
--- api/models.py
+++ api/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 
-class Task(models.Model):
+class AbstractModel(models.Model):
+    crated_at = models.DateTimeField(default=timezone.now)
+
+    # Para que sea una clase abstracta debe contener la clase Meta de forma interna
+    class Meta:
+        abstract = True
+
+class Task(AbstractModel):
     title = models.CharField(max_length=180)
     description = models.TextField(null=True, blank=True)
     priority = models.IntegerField(
         choices=((0, 'low'), (1, 'medium'), (2, 'high'))
     )
-    crated_at = models.DateTimeField(default=timezone.now)
     expire_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+class Comment(AbstractModel):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='comment_task'
+    )
+    comment = models.TextField()
